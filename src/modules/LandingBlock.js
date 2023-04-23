a38c7958c06abbacb491c135233a2de859911196
Eino Pitkälä <eiccabot@gmail.com>
2023-04-23 17:23:00 +0300

    removed not usable nav

--- src/modules/LandingBlock.js
+++ src/modules/LandingBlock.js
@@ -3,13 +3,15 @@ import Button from "./Button";
 export default function LandingBlock() {
   return (
     <div className="landing-block">
-      <h1 className="companyName">
-        IT-Apua asiantuntevalta 14-vuotiaalta nuorelta yrittäjältä.
-      </h1>
-      <h2 className="marketing-word">
-        Hanki mielenrauhaa laitteidesi toimintavarmuuteen. Valitse pienyrityksen
-        apu. Tuet myös samalla nuoren pojan unelmia ja haaveita.
-      </h2>
+      <div className="text-block">
+        <h1 className="companyName">
+          IT-Apua asiantuntevalta 14-vuotiaalta nuorelta yrittäjältä.
+        </h1>
+        <h2 className="marketing-word">
+          Hanki mielenrauhaa laitteidesi toimintavarmuuteen. Valitse pienyrityksen
+          apu. Tuet myös samalla nuoren pojan unelmia ja haaveita.
+        </h2>
+      </div>
       <div className="button-row">
         <Button buttonText="Palvelut" url="https://google.com/" />
         <Button buttonText="Lue lisää" url="https://google.com/" />
