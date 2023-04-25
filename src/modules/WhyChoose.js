a38f35d0e13c7a0c365b89bebd64b479a73f67c7
Eino Pitkälä <eiccabot@gmail.com>
2023-04-25 21:16:30 +0300

    Nice card design and colors

--- src/modules/WhyChoose.js
+++ src/modules/WhyChoose.js
@@ -1,4 +1,4 @@
-import { FaCreditCard, FaPeopleArrows } from 'react-icons/fa';
+import { FaCreditCard, FaPeopleArrows, FaClock, FaQuestion } from 'react-icons/fa';
 
 import Card from './Card';
 export default function WhyChoose() {
@@ -15,6 +15,16 @@ export default function WhyChoose() {
                     icon={<FaPeopleArrows />}
                     title="Luotettavuus"
                     content="Olen luotettava ja rehellinen yrittäjä, joka haluaa tarjota asiakkailleen parasta mahdollista palvelua. Olen ainut yrityksen työntekijä, joten saat aina minun palvelukseni."
+                />
+                <Card
+                    icon={<FaClock />}
+                    title="Luotettavuus"
+                    content="Nuorena ja ketteränä yrittäjänä minulla on aikaa asiakkailleni ja tilanteiden vaatiessa pystyn sopeutumaan nopeasti muuttuviin tilanteisiin."
+                />
+                <Card
+                    icon={<FaQuestion />}
+                    title="Lorem ipsum"
+                    content="Lorem ipsun dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget aliquam ultricies, nisl nisl tincidunt nisl, quis ultrices nisl nisl nec nisl."
                 />
             </div>
         </div>
