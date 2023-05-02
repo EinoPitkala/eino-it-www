17bc5763c6e2d67d8eec481f6f748f2467e404a4
Eino Pitkälä <eiccabot@gmail.com>
2023-05-02 21:08:56 +0300

    Added hidden website

---
+++ src/pages/SomeKayttaytyminen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SomeKayttaytyminen() {
+  return (
+    <div className="some-kayttaytyminen">
+      <h1>Somekäyttäytymisen neuvonta</h1>
+      <h2>Kohderyhmä: keski-ikäiset</h2>
+      <p>
+        Sisältää: Mitä kannatta julkaista?, Kielioppisäännöt somessa, Mitä
+        kannattaa clickata ja mitä ei? Kannattaako jokaiseen iltapäivälehden
+        otsikkoon uskoa? Miten somen  algoritmit katsoo mitä olet ennenkin clickannut?
+      </p>
+    </div>
+  );
+}
