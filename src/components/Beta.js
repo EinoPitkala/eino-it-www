937adf590edefef0f7714da4996341118b51165c
Eino Pitkälä <eiccabot@gmail.com>
2023-05-02 18:21:35 +0300

    Added beta warning to the top page.

---
+++ src/components/Beta.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BetaWarning() {
+  return (
+    <div className="beta-warning">
+        <p>Nettisivut on beta vaiheessa ja niitä kehitetään koko ajan lisää.</p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Beta from "./components/Beta";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <Beta />
       <NavBar />
       <div className="content">
         <Routes>
