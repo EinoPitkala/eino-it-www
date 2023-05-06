6f2ff5f41652fc40ac3cd9b836ff202d2c8753b7
Eino Pitkälä <eiccabot@gmail.com>
2023-05-06 17:36:52 +0300

    Fixed one typo

--- src/components/Beta.js
+++ src/components/Beta.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BetaWarning() {
   return (
     <div className="beta-warning">
-        <p>Nettisivut on beta vaiheessa ja niitä kehitetään koko ajan lisää.</p>
+        <p>Nettisivut on betavaiheessa ja niitä kehitetään koko ajan lisää.</p>
     </div>
   )
 }
